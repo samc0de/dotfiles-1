0a1911fd7da00cb65ca260064c84da719711db3c
samc0de <sssam.sam@gmail.com>
2023-07-15 01:17:35 +0530

    Update vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,3 +1,10 @@
-mkdir -p ~/.vim
+set -e
+
+# Assumes vim w/ python3, curl & git installed.
+mkdir -p ~/.vim ~/.vimdir
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+curl https://raw.githubusercontent.com/samc0de/dotfiles-1/main/.vimrc >> ~/.vimrc
+
+vim -c PluginUpdate
